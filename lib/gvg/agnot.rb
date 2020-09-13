0d54325d03802b7c3ff9494696990eceadbe5b3a
Sailaja Nagireddy <sailaja-nagireddy@users.noreply.github.com>
2020-09-13 22:05:45 +0530

    - Generated VoiceXML using nokogiri builder.
    This commit includes changes to accommodate nokogiri gem
    for the creation of the VoiceXML document.
    
    + Use nokogiri builder to generate voicexml audio tags.
    + Determining the method dynamically from the state type.
    + Updated parser to refer to the json file created in test.
    + Added sample vxml data.

--- lib/gvg/agnot.rb
+++ lib/gvg/agnot.rb
@@ -1,26 +1,29 @@
 require 'aasm'
+require 'nokogiri'
 
-def audio(src, text)
-  return <<-EOS
-    <block>
-      <prompt>
-        <audio src="/media/audio/#{src}">#{text}</audio>
-      </prompt>
-    </block>
-  EOS
+def audio(audio)
+  parent = @builder.doc.at('vxml')
+  builder = Nokogiri::XML::Builder.with(parent) do |xml|
+    xml.block do
+      xml.prompt do
+        xml.audio(src: audio['source']) do
+          xml.text(audio['text'])
+        end
+      end
+    end
+  end
+
+  return builder.to_xml
 end
 
 def open
-  return <<-EOS
-    <?xml version="1.0" encoding="ISO-8859-1"?>
-    <vxml version="2.0" lang="en">
-  EOS
+  @builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+    xml.vxml(version: 2.0, lang: 'en')
+  end
 end
 
 def close
-  return <<-EOS
-    </vxml>
-  EOS
+  puts @builder.to_xml
 end
 
 class Gvg::Agnot
@@ -32,17 +35,17 @@ class Gvg::Agnot
     state :done
 
     event :start do
-      transitions to: :waiting, after: Proc.new {|*args| puts open(*args)}
+      transitions to: :waiting, after: Proc.new {|*args| open(*args)}
     end
 
     event :stop do
-      transitions to: :done, after: Proc.new {|*args| puts close(*args)}
+      transitions to: :done, after: Proc.new {|*args| close(*args)}
     end
 
     event :say do
-      transitions from: :waiting, to: :saying, after: Proc.new {|*args| puts audio(*args)}
-      transitions from: :listening, to: :saying, after: Proc.new {|*args| puts audio(*args)}
-      transitions from: :saying, to: :saying, after: Proc.new {|*args| puts audio(*args)}
+      transitions from: :waiting, to: :saying, after: Proc.new {|*args| audio(*args)}
+      transitions from: :listening, to: :saying, after: Proc.new {|*args| audio(*args)}
+      transitions from: :saying, to: :saying, after: Proc.new {|*args| audio(*args)}
     end
 
     event :listen do
--- lib/gvg/parser.rb
+++ lib/gvg/parser.rb
@@ -2,50 +2,7 @@ require 'json'
 
 class Gvg::Parser
   def parse
-    automaton = %{
-      {
-        "id": "1",
-        "name": "State Machine 1",
-        "type": "test",
-        "description": "A test state machine.",
-        "states": [
-          {
-            "id": "1",
-            "sequence": "1",
-            "name": "State 1",
-            "type": "say",
-            "description": "A test state.",
-            "audio": {
-              "source": "https://example.com/media/audio/1.mp3",
-              "text": "I am in state 1."
-            }
-          },
-          {
-            "id": "2",
-            "sequence": "2",
-            "name": "State 2",
-            "type": "say",
-            "description": "A test state.",
-            "audio": {
-              "source": "https://example.com/media/audio/2.mp3",
-              "text": "I am in state 2."
-            }
-          },
-          {
-            "id": "3",
-            "sequence": "3",
-            "name": "State 3",
-            "type": "say",
-            "description": "A test state.",
-            "audio": {
-              "source": "https://example.com/media/audio/3.mp3",
-              "text": "I am in state 3."
-            }
-          }
-        ]
-      }
-    }
-
+    automaton = File.read('test/state-machine-1.json')
     return JSON.parse(automaton)
   end
 end
